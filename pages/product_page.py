cdc6c5b62231173b07f0b37055369054ba3ab323
Natalia Popova <sverdlova3@gmail.com>
2022-10-13 14:14:24 +0500

    edit func item_added_in_basket and price_in_basket_equal_item_price

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,7 +8,13 @@ class ProductPage(BasePage):
         button.click()
 
     def item_added_in_basket(self):
-        pass
+        item = self.browser.find_element(*ProductPageLocators.BOOK_NAME)
+        item_in_basket = self.browser.find_element(*ProductPageLocators.BOOK_NAME_IN_BASKET)
+        assert item.text == item_in_basket.text, "Item not in the basket"
+        print(f"{item_in_basket.text} in the basket")
 
     def price_in_basket_equal_item_price(self):
-        pass
+        price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE)
+        price_in_basket = self.browser.find_element(*ProductPageLocators.COST_BASKET)
+        assert price.text == price_in_basket.text, "Price incorrect in the basket"
+        print(f"Price {price_in_basket.text} is correct")
