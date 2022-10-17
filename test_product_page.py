0a8a292cd8304dc5e7b1bfad250826608cacf270
Natalia Popova <sverdlova3@gmail.com>
2022-10-17 15:14:45 +0500

    add parametrize url to func

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,11 @@
+import pytest
+
 from .pages.product_page import ProductPage
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+
+@pytest.mark.parametrize('offer', [n if n != 7 else pytest.param(n, marks=pytest.mark.xfail) for n in range(10)])
+def test_guest_can_add_product_to_basket(browser, offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer}"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_in_basket()
