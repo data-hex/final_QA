23d2bd08d62397005b3cb90cb54b8a137c02497c
Natalia Popova <sverdlova3@gmail.com>
2022-10-19 13:14:33 +0500

    add test_guest_cant_see_success_message_after_adding_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 from .pages.product_page import ProductPage
 
 
@@ -8,7 +9,32 @@ def test_guest_can_add_product_to_basket(browser, offer):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer}"
     page = ProductPage(browser, link)
     page.open()
+    # page.should_not_be_success_message()
     page.add_product_in_basket()
     page.solve_quiz_and_get_code()
     page.item_added_in_basket()
     page.price_in_basket_equal_item_price()
+    # page.success_message_should_disappear()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_in_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_in_basket()
+    page.success_message_should_disappear()
