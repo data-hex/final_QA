eb4fd5978ca064bd625f9ce67c98a86e18d87c7d
Natalia Popova <sverdlova3@gmail.com>
2022-10-20 14:57:05 +0500

    add test_guest_can_go_to_login_page_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,5 @@
 import pytest
 
-
 from .pages.product_page import ProductPage
 
 
@@ -15,6 +14,7 @@ def test_guest_can_add_product_to_basket(browser, offer):
     page.item_added_in_basket()
     page.price_in_basket_equal_item_price()
     # page.success_message_should_disappear()
+
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
@@ -31,6 +31,7 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_success_message()
 
+
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -38,3 +39,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.add_product_in_basket()
     page.success_message_should_disappear()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
